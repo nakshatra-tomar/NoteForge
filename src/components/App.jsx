9dafe372f24d4bac6fdf8253784aadfb7f6be80e
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-02-03 23:31:22 +0530

    components

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header.jsx"
+
+function App() {
+    return <div>
+        <h1>
+            App working
+        </h1>
+    </div>
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from  "react";
+
+function Header(){
+     return <header>
+        <h1>NoteForge</h1>
+     </header>
+}
+
+export default Header;
