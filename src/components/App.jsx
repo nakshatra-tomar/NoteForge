612513e35ada616a85a25e24adcceb276bdb7de4
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-02-07 00:01:57 +0530

    added notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
+
+console.log(notes);
 
 function App() {
     return <div>
---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Random Fact 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum ligula at enim feugiat, vel interdum odio vehicula."
+  },
+  {
+    key: 2,
+    title: "Interesting Observation",
+    content:
+      "Suspendisse potenti. Sed varius odio sed libero tincidunt, non sagittis metus vehicula. Donec id nulla at lectus elementum vehicula."
+  },
+  {
+    key: 3,
+    title: "Programming Joke",
+    content:
+      "Phasellus efficitur purus sed justo eleifend, in laoreet libero tristique. Fusce gravida velit sit amet libero bibendum, non commodo risus volutpat."
+  },
+  {
+    key: 4,
+    title: "Tech Trivia",
+    content:
+      "Vestibulum id justo quis velit efficitur elementum. Nulla facilisi. Morbi consequat odio nec neque vestibulum, nec vestibulum justo varius."
+  }
+];
+
+export default notes;
